006b7d6ea453fa6b07181869a15ff2e0036a366c
Derek Vance <dvance@g.ucla.edu>
2021-04-19 11:48:55 -0700

    Integration tests work on GCloud
    Change-Id: I7fc9ea8c9a7d1d43d2f492719a9b90911d60842c

--- docker/coverage.Dockerfile
+++ docker/coverage.Dockerfile
@@ -4,8 +4,13 @@ FROM team-juzang:base as builder
 
 # Share work directory
 COPY . /usr/src/project
+
+WORKDIR /usr/src/project/build
+RUN cmake ..
+RUN make
+
 WORKDIR /usr/src/project/build_coverage
 
 # Build and test
 RUN cmake -DCMAKE_BUILD_TYPE=Coverage ..
-RUN make coverage
+RUN env CTEST_OUTPUT_ON_FAILURE=1 make coverage
--- tests/integration.sh
+++ tests/integration.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-
+DIR=`dirname $0`
+PWD=`pwd`
 PORT=8080
-
+if [[ ! $PWD =~ $DIR$ ]]; then
+  echo "wrong directory: must run integration.sh from tests/"
+  exit 1
+fi
 echo "port ${PORT};" > tmp.config
-
 ../build/bin/webserver tmp.config &
-curl -sSi http://localhost:$PORT/
-EXIT=$?
-
-rm -f tmp.config
+if [[ $? -eq 0 ]]; then
+  curl -sS http://localhost:$PORT/ > tmp.out
+  diff -w regression.out tmp.out
+  EXIT=$?
+else
+  echo "Failed to start webserver; exit code: ${$?}"
+  EXIT=1
+fi
+rm -f tmp.config tmp.out
 kill $! > /dev/null 2>&1
 exit $EXIT
