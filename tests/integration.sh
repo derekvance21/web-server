76718141d65c257caf962ae81adea48f82125d51
Derek Vance <dvance@g.ucla.edu>
2021-04-19 00:02:17 -0700

    Integration should work now
    Change-Id: I2b6fa51a1531a2a522dee905e6e8825c7b9f21d4

--- tests/integration.sh
+++ tests/integration.sh
@@ -4,24 +4,18 @@ DIR=`dirname $0`
 PWD=`pwd`
 PORT=8080
 
-# if [[ ! $PWD =~ $DIR$ ]]; then
-#   echo "wrong directory: must run integration.sh from tests/"
-#   exit 1
-# fi
-
-EXIT=0
+if [[ ! $PWD =~ $DIR$ ]]; then
+  echo "wrong directory: must run integration.sh from tests/"
+  exit 1
+fi
 
 echo "port ${PORT};" > tmp.config
 
 ../build/bin/webserver tmp.config &
-if [[ $? -eq 0 ]]; then
-  curl -sSi http://localhost:$PORT/ > tmp.out
-  # diff regression.out tmp.out
-  # EXIT=$?
-else
-  echo "Failed to start webserver; exit code: ${$?}"
-  EXIT=1
-fi
+sleep 0.2
+echo "Hello, world!" | nc localhost 8080 -w 1 > tmp.out
+diff -Z regression.out tmp.out
+EXIT=$?
 
 rm -f tmp.config tmp.out
 kill $! > /dev/null 2>&1
