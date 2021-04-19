353afda5484b134dcf6cd0598385f6b538cb482b
Derek Vance <dvance@g.ucla.edu>
2021-04-19 00:14:32 -0700

    This might work
    Change-Id: I4823d9fbfaa9daab0e91b27250c6960b7d453525

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-DIR=`dirname $0`
-PWD=`pwd`
 PORT=8080
-
-if [[ ! $PWD =~ $DIR$ ]]; then
-  echo "wrong directory: must run integration.sh from tests/"
-  exit 1
-fi
 
 echo "port ${PORT};" > tmp.config
 
 ../build/bin/webserver tmp.config &
-sleep 0.2
+sleep 1
 echo "Hello, world!" | nc localhost 8080 -w 1 > tmp.out
-diff -Z regression.out tmp.out
+diff -w regression.out tmp.out
 EXIT=$?
 
 rm -f tmp.config tmp.out
