1ac867400b5b0ee6dfc31a49ae6b9dd623764990
Derek Vance <dvance@g.ucla.edu>
2021-04-19 00:27:54 -0700

    Scrapped regression testing
    Change-Id: I0e73b9853eeb6ad610e6ade003364bc11f2c71d5

--- tests/integration.sh
+++ tests/integration.sh
@@ -5,11 +5,9 @@ PORT=8080
 echo "port ${PORT};" > tmp.config
 
 ../build/bin/webserver tmp.config &
-sleep 1
-echo "Hello, world!" | nc localhost 8080 -w 1 > tmp.out
-diff -w regression.out tmp.out
+curl -sSi http://localhost:$PORT/
 EXIT=$?
 
-rm -f tmp.config tmp.out
+rm -f tmp.config
 kill $! > /dev/null 2>&1
 exit $EXIT
